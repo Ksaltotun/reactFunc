cfb6cbad6ca7dcd1a51552d37433cda46c4ea861
rmishchanka <rmishchanka@gomel.iba.by>
2019-12-02 02:01:53 +0300

    add spiner

--- src/app/GuestsTable/GuestsTable.js
+++ src/app/GuestsTable/GuestsTable.js
@@ -2,12 +2,18 @@ import React, {useState, useEffect} from "react";
 import User from "../User";
 import "./guestsTable.css";
 import {useSelector} from "react-redux";
+import Spiner from "../Spiner";
 
 export const GuestsTable = () => {
-  const peoples = useSelector(store => store.peoples);
+  const {peoples, spiner} = useSelector(store => store);
 
   const peoplesList = peoples.map(pipl => {
     return <User key={pipl.id} id={pipl.id} name={pipl.name} age={pipl.age} />;
   });
-  return <div className="jumbotron userList">{peoplesList}</div>;
+  return (
+    <div className="jumbotron userList">
+      {peoplesList}
+      {spiner ? <Spiner /> : null}
+    </div>
+  );
 };
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,4 +1,4 @@
-export const reducer = (state = {peoples: []}, action) => {
+export const reducer = (state = {peoples: [], spiner: false}, action) => {
   const nextId =
     state.peoples.length > 0 ? state.peoples[state.peoples.length - 1].id + 1 : 1;
   switch (action.type) {
@@ -9,15 +9,33 @@ export const reducer = (state = {peoples: []}, action) => {
           age: action.age,
           id: nextId,
         }),
+        spiner: false,
+      };
+    case "PUSH_PEOPLE_REQUEST":
+      return {
+        peoples: state.peoples,
+        spiner: true,
+      };
+    case "DELETE_PEOPLE_REQUEST":
+      return {
+        peoples: state.peoples,
+        spiner: true,
       };
     case "DELETE_PEOPLE_SUCCES":
-      console.log(action, '++');
       return {
         peoples: state.peoples.filter(item => item.id !== action.id),
+        spiner: false,
       };
+    case "GET_PEOPLE_REQUEST":
+      return {
+        peoples: state.peoples,
+        spiner: true,
+      };
+
     case "GET_PEOPLE_SUCCES":
       return {
         peoples: action.peoples,
+        spiner: false,
       };
     default:
       return state;
